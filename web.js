4ec9524effe720c8c7ff84d651eee66c980352cc
Michael Homsey <m.s.homsey@gmail.com>
2013-07-10 00:34:38 +1000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile('index.html',  function (err, data) {
+        var buffer = new Buffer(data);
+	response.send(buffer.toString('utf-8'));
+  })
 });
 
 var port = process.env.PORT || 5000;
