a18bb37bd1a90d9bf7a0ffba0a6e84f01768789d
Michael Homsey <m.s.homsey@gmail.com>
2013-07-11 15:05:45 +1000

    update 3

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,8 +66,17 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to access')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if (program.file || program.url) {
+	if ( program.url) {
+console.log ('using url mode ' + program.url);
+		// do something with th URL
+	} else { // is just a file, nor url
+console.log ('using file mode ' + program.file);
+	    var checkJson = checkHtmlFile(program.file, program.checks);
+	}
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
